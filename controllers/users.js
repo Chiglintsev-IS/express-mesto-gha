936f8ab9469aff93dfb2dfbad57d099c6a7c61e0
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2022-12-11 20:47:00 +0100

    add castError validation

--- controllers/users.js
+++ controllers/users.js
@@ -17,6 +17,10 @@ module.exports.getUserById = (req, res) => {
         res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
         return;
       }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при поиске пользователя.' });
+        return;
+      }
       res.status(500).send({ message: err.message });
     });
 };
