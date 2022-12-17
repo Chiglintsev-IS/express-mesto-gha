cb16c82a6e9c0e0db755cb47098ad5b5d21fbd1a
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2022-12-17 21:37:53 +0100

    change export

--- constants.js
+++ constants.js
@@ -1,13 +1,13 @@
-export const userErrors = {
+module.exports.userErrors = {
   notFoundId: 'Пользователь по указанному _id не найден.',
   CastError: 'Переданы некорректные данные при поиске пользователя.',
   ValidationError: 'Переданы некорректные данные при создании пользователя.',
 };
 
-export const cardErrors = {
+module.exports.cardErrors = {
   notFoundId: 'Карточка с указанным _id не найдена.',
   CastError: 'Передан несуществующий _id карточки.',
   ValidationError: 'Переданы некорректные данные при создании карточки.',
 };
 
-export const serverError = { common: 'Ошибка на сервере' };
+module.exports.serverError = { common: 'Ошибка на сервере' };
